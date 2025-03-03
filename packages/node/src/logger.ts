20d98fdf0e5b6136ab1125a9a78d09111df797ea
Sacha Froment <sfroment42@gmail.com>
2025-03-03 15:54:36 +0100

    chore: add comment
    Signed-off-by: Sacha Froment <sfroment42@gmail.com>

--- packages/node/src/logger.ts
+++ packages/node/src/logger.ts
@@ -1,5 +1,6 @@
 import { ILogger } from "@ts-drp/logger";
 
+// TODO: replace with the actual logger in each place that need a logger this is just a hack by the mean time to fix the circular dependency
 export const log: ILogger = {
 	trace: () => {},
 	debug: () => {},
