12627b1800a10c39cae78edfc62183aee99392f5
Sacha Froment <sfroment42@gmail.com>
2025-03-13 16:49:08 +0100

    chore: better calling
    Signed-off-by: Sacha Froment <sfroment42@gmail.com>

--- packages/network/src/node.ts
+++ packages/network/src/node.ts
@@ -254,11 +254,11 @@ export class DRPNetworkNode implements DRPNetworkNodeInterface {
 
 	async isDialable(callback?: () => void | Promise<void>): Promise<boolean> {
 		let dialable = await this._node?.isDialable(this._node.getMultiaddrs());
+		if (!callback) return dialable ?? false;
 		if (dialable) {
-			if (callback) await callback();
+			await callback();
 			return true;
 		}
-		if (!callback) return false;
 
 		const checkDialable = async (): Promise<void> => {
 			dialable = await this._node?.isDialable(this._node.getMultiaddrs());
