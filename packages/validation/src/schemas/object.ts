c97ed896fc72dfaa8fdfab99ea3b136cba07a0cd
trungnotchung <trung09072004@gmail.com>
2025-03-25 23:53:02 +0700

    update schema

--- packages/validation/src/schemas/object.ts
+++ packages/validation/src/schemas/object.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const CreateObjectOptionsSchema = z.object({
 	peerId: z.string().min(1, "PeerId is required"),
-	id: z.string().min(1, "Id is required"),
+	id: z.string().min(1, "Id is required").optional(),
 });
 
 export const NodeConnectObjectOptionsSchema = z.object({
