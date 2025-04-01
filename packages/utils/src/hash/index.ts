0c6fb0bddc1684d9f86d7134ae3a1a7ae617be16
Sacha Froment <sfroment42@gmail.com>
2025-04-01 16:19:57 +0200

    chore: attestation
    Signed-off-by: Sacha Froment <sfroment42@gmail.com>

--- packages/utils/src/hash/index.ts
+++ packages/utils/src/hash/index.ts
@@ -1,9 +1,9 @@
 import { sha256 } from "@noble/hashes/sha2";
 import { bytesToHex } from "@noble/hashes/utils";
-import { type Hash, Vertex } from "@ts-drp/types";
+import type { Hash, Operation } from "@ts-drp/types";
 
-export function computeHash(vertex: Vertex): Hash {
-	const serialized = Vertex.encode(vertex).finish();
+export function computeHash(peerId: string, operation: Operation | undefined, deps: Hash[], timestamp: number): Hash {
+	const serialized = JSON.stringify({ operation, deps, peerId, timestamp });
 	const hash = sha256.create().update(serialized).digest();
 	return bytesToHex(hash);
 }
